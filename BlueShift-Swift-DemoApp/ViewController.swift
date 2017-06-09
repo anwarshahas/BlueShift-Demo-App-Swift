3037c519fbe2a1bb4f0506beaedd5b1e733d6672
shahas <shahas@flogic.in>
2017-06-09 15:18:16 +0530

    Initial Commit

---
+++ BlueShift-Swift-DemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueShift-Swift-DemoApp
+//
+//  Created by shahas kp on 09/06/17.
+//  Copyright © 2017 shahas kp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
